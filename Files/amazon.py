a8a1895de13d8896824c8ab3a4e8230750db442c
Ulvi Allahverdiyev <ulvi3d@gmail.com>
2023-02-18 13:13:01 +0400

    first version

---
+++ Files/amazon.py
@@ -0,0 +1,53 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+
+class amazon():
+
+    def add_to_cart(self):
+        s = Service("D:\Drivers\chromedriver.exe")
+        driver = webdriver.Chrome(service=s)
+        driver.implicitly_wait(3)
+
+        driver.get("https://www.amazon.com/")
+        driver.maximize_window()
+
+
+        # search for asus laptop
+        search_bar = driver.find_element(By.XPATH, "(//input[@id='twotabsearchtextbox'])[1]")
+        search_bar.click()
+        search_bar.send_keys("asus laptop")
+        search_bar.send_keys(Keys.ENTER)
+
+        # sort by Newest Arrivals
+        driver.find_element(By.XPATH, "(//span[@class='a-dropdown-label'])[1]").click()
+        driver.find_element(By.XPATH, "(//a[normalize-space()='Newest Arrivals'])[1]").click()
+
+        # select second product from the list
+        driver.find_element(By.PARTIAL_LINK_TEXT, "ASUS").click()
+        time.sleep(3)
+
+        # change the delivery country to US
+        driver.find_element(By.ID, "contextualIngressPtPin").click()
+        dropdown = driver.find_element(By.XPATH, "//span[@id='GLUXCountryValue']")
+        actions = ActionChains(driver)
+        actions.double_click(dropdown).perform()
+        time.sleep(1)
+        driver.find_element(By.ID, "GLUXCountryList_234").click()
+        driver.refresh()
+
+        # add to cart
+        driver.find_element(By.XPATH, "//input[@id='add-to-cart-button']").click()
+        driver.find_element(By.XPATH, "//input[@aria-labelledby='attachSiNoCoverage-announce']").click()
+        time.sleep(1)
+        driver.refresh()
+        time.sleep(1)
+        driver.find_element(By.XPATH, "//span[normalize-space()='Cart']").click()
+
+        time.sleep(3)
+
+start_test = amazon()
+start_test.add_to_cart()
